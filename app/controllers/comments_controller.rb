4356f51768c4b908b0fbcaeccb8aea4c9d90b6f4
Nate Delage <nate@natedelage.com>
2013-07-19 14:27:52 -0500

    Rails ujs style ajax, rendering .js.erb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(params[:comment])
-    if @comment.save
-      redirect_to @comment.post
-    else
-      flash[:error] = "Unable to save post"
-      redirect_to @comment.post
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          redirect_to @comment.post
+        else
+          flash[:error] = "Unable to save post"
+          redirect_to @comment.post
+        end
+      end
+
+      format.js do
+        # for the js format, app/comments/create.js.erb will be rendered
+        # by default. no need to call render
+        @errors = []
+        if !@comment.save
+          errors << "Unable to save post"
+        end
+      end
     end
   end
 end
-
